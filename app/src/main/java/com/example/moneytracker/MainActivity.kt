0d09f2c88c225557a4cb362c70f961784b99428d
jeniok360v <gerhugwsif@gmail.com>
2021-05-02 20:47:51 +0200

    wallets recyclerview

--- app/src/main/java/com/example/moneytracker/MainActivity.kt
+++ app/src/main/java/com/example/moneytracker/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.moneytracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val walletRecyclerView = findViewById<RecyclerView>(R.id.walletRecyclerView)
+        walletRecyclerView.layoutManager = LinearLayoutManager(this)
+        //walletRecyclerView.adapter = WalletRecyclerAdapter(listOf("e_wallet_1", "e_wallet_2", "wallet_3"))
+        walletRecyclerView.adapter = WalletRecyclerAdapter(generateData())
+    }
+
+
+    fun generateData(): List<String>{
+        val data: MutableList<String> = ArrayList()
+        for(i in 1..10){
+            data.add("wallet_" + i.toString())
+        }
+        return data
     }
 }
---
+++ app/src/main/java/com/example/moneytracker/WalletRecyclerAdapter.kt
@@ -0,0 +1,50 @@
+package com.example.moneytracker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+
+class WalletRecyclerAdapter(private val dataSet: List<String>) :
+    RecyclerView.Adapter<WalletRecyclerAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+
+            // Define click listener for the ViewHolder's View.
+            textView = view.findViewById(R.id.wallet_item_title)
+            view.setOnClickListener{
+                Toast.makeText(view.context, "position: " + layoutPosition + ", title: " + textView.text, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.wallet_item, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = dataSet[position]
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
+
+}
