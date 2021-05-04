747a9cdddff7036f8202aa2762fbfb3bcb9e906d
jeniok360v <gerhugwsif@gmail.com>
2021-05-05 00:23:17 +0200

    SQLite Room test

--- app/src/main/java/com/example/moneytracker/MainActivity.kt
+++ app/src/main/java/com/example/moneytracker/MainActivity.kt
@@ -2,18 +2,39 @@ package com.example.moneytracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.moneytracker.database.Wallet
+import com.example.moneytracker.database.WalletDatabase
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var db : WalletDatabase//? = WalletDatabase.getInstance(applicationContext)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //db test
+        GlobalScope.launch {
+            try{
+                db = WalletDatabase.getInstance(applicationContext)!!
+            }catch(e: Exception) {
+                Log.d("wallet_insert", e.message.toString())
+            }
+
+            val s = Wallet(0, "testString")
+            db.walletDAO().insertAll(s)
+        }
         val walletRecyclerView = findViewById<RecyclerView>(R.id.walletRecyclerView)
         walletRecyclerView.layoutManager = LinearLayoutManager(this)
         //walletRecyclerView.adapter = WalletRecyclerAdapter(listOf("e_wallet_1", "e_wallet_2", "wallet_3"))
         walletRecyclerView.adapter = WalletRecyclerAdapter(generateData())
+
     }
 
 
--- app/src/main/java/com/example/moneytracker/database/WalletDatabase.kt
+++ app/src/main/java/com/example/moneytracker/database/WalletDatabase.kt
@@ -1,10 +1,36 @@
 package com.example.moneytracker.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
 @Database(entities = [Wallet::class], version = 1)
 abstract class WalletDatabase: RoomDatabase() {
     abstract fun walletDAO(): WalletDAO
+
+    companion object
+    {
+        private var INSTANCE: WalletDatabase? = null
+
+        fun getInstance(context: Context): WalletDatabase?
+        {
+            if (INSTANCE == null)
+            {
+                synchronized(WalletDatabase::class)
+                {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext,
+                        WalletDatabase::class.java, "wallet.db").build()
+                }
+            }
+            return INSTANCE
+        }
+
+        fun destroyInstance()
+        {
+            INSTANCE = null
+        }
+    }
+
 }
