453afdf644e192c186f3e6cc7d7428a4b8d39a11
jeniok360v <gerhugwsif@gmail.com>
2021-05-05 00:25:57 +0200

    deleteWallet

--- app/src/main/java/com/example/moneytracker/database/WalletDAO.kt
+++ app/src/main/java/com/example/moneytracker/database/WalletDAO.kt
@@ -1,6 +1,7 @@
 package com.example.moneytracker.database
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 
@@ -13,5 +14,8 @@ interface WalletDAO {
 
     @Insert
     fun insertAll(vararg wallet : Wallet)
+
+    @Delete
+    fun deleteWallet(wallet: Wallet)
 
 }
